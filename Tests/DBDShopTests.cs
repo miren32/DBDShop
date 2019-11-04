0226d1c3c6e22de0866d1ee0c1ee9ec9e5ac56d3
Nadia <nreina003@ikasle.ehu.eus>
2019-11-04 11:58:35 +0100

    intento

--- Tests/DBDShopTests.cs
+++ Tests/DBDShopTests.cs
@@ -28,13 +28,40 @@ namespace Tests
             Assert.IsTrue(products.Count == 2);
         }
         [TestMethod]
-        public void MyOhterTest()
+        public void TestSoldOut()
         {
-            //Connect to the test database
-            //Client client = new Client("NLphb4HrH0", "NLphb4HrH0", "VM8GYV3qZ7");
+            // Connect to the test database
+            Client client = new Client("pBRMsmc7h2", "pBRMsmc7h2", "mQvsG2x5NR");
+            //Get all the existing products
+            List<Product> products = client.GetProducts();
+
+            client.DeleteProducts(products);
+            //Check we deleted all the products
+            products = client.GetProducts();
+            Assert.IsTrue(products.Count == 0);
+
+            // MAL hay que ponerle mi lista no esa
+
+            //Insert test data
+            client.InsertTestData();
+            //Check they were correctly inserted
+            products = client.GetProducts();
+            Assert.IsTrue(products.Count == 2);
+
+
+        }
+
+        [TestMethod]
+        public void TestAddProduct()
+        {
             
-            //Any testing you need to do
-            //....
+            // Connect to the test database
+            Client client = new Client("pBRMsmc7h2", "pBRMsmc7h2", "mQvsG2x5NR");
+            //Get all the existing products
+            List<Product> products = client.GetProducts();
+
+
+
         }
     }
 }
