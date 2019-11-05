c486754e104acf8b2e99384132957f9229ad1568
Nadia <nreina003@ikasle.ehu.eus>
2019-11-05 12:15:14 +0100

    I need to change brange

--- Tests/DBDShopTests.cs
+++ Tests/DBDShopTests.cs
@@ -53,11 +53,23 @@ namespace Tests
         [TestMethod]
         public void TestAddProduct()
         {
-            
             // Connect to the test database
             Client client = new Client("pBRMsmc7h2", "pBRMsmc7h2", "mQvsG2x5NR");
             //Get all the existing products
             List<Product> products = client.GetProducts();
+
+            client.DeleteProducts(products);
+            //Check we deleted all the products
+            products = client.GetProducts();
+            Assert.IsTrue(products.Count == 0);
+
+            // MAL hay que ponerle mi lista no esa
+
+            //Insert test data
+            client.AddProduct(Product);
+            //Check they were correctly inserted
+            products = client.GetProducts();
+            Assert.IsTrue(products.Count == 2);
 
 
 
